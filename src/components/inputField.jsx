6b22fae547564cc1bc07524660c28e24314b0ec7
akshay.rein-at-908805445373 <waizaeer4@gmail.com>
2022-05-16 14:55:18 +0530

    Test

--- src/components/inputField.jsx
+++ src/components/inputField.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
 
 export default function InputField(props) {
-  const { value, fetchValue = () => {} } = props;
+  const { value, fetchValue = () => {}, hint = "Some Text", type = "" } = props;
 
   function fieldValueChange(event) {
     fetchValue(event.target.value);
   }
 
+  function contentGen(type) {
+    switch (type) {
+      case "checkbox":
+        return <input value={value} onChange={fieldValueChange} className="input-field-checkbox" type="checkbox" />;
+      default:
+        return <input value={value} onChange={fieldValueChange} placeholder={hint} className="input-field" />;
+    }
+  }
+
   return (
-    <span>
-      <input value={value} onChange={fieldValueChange} />
+    <span className={type === "checkbox" ? "align-horizontal" : "normal"}>
+      {contentGen(type)}
+      {type === "checkbox" && <p className="input-field-checkbox-hint">{hint}</p>}
     </span>
   );
 }
--- src/screens/login.jsx
+++ src/screens/login.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import { Button, InputField } from "../components";
 
 const LoginPage = () => {
-  const [login, setLogin] = useState();
+  const [login, setLogin] = useState({ userName: "", email: "", password: "", confirmPass: "", policyAggre: false });
+
+  useEffect(() => {
+    console.log("Login Component Loaded");
+  }, [login]);
 
   function buttonClicked() {
-    alert("Clicked Button from Login");
+    alert(login);
   }
 
   return (
@@ -14,8 +18,16 @@ const LoginPage = () => {
       <div className="loginpage-cc">
         <p>LOGO</p>
         <p>Login with your Credentails</p>
-        <p>{login}</p>
-        <InputField value={login} fetchValue={(val) => setLogin(val)} />
+        <InputField value={login.userName} fetchValue={(val) => setLogin({ ...login, userName: val })} hint="UserName" />
+        <InputField value={login.email} fetchValue={(val) => setLogin({ ...login, email: val })} hint="Email" />
+        <InputField value={login.password} fetchValue={(val) => setLogin({ ...login, password: val })} hint="Password" />
+        <InputField value={login.confirmPass} fetchValue={(val) => setLogin({ ...login, confirmPass: val })} hint="Confirm Password" />
+        <InputField
+          value={login.policyAggre}
+          fetchValue={(val) => setLogin({ ...login, policyAggre: val })}
+          hint="Agree to Our terms and Conditions"
+          type="checkbox"
+        />
         <Button action={buttonClicked} title="Login" />
       </div>
     </div>
