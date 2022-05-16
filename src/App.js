40752800bb3cd9e68f285c215aff1b09544ec170
akshay.rein-at-908805445373 <waizaeer4@gmail.com>
2022-05-16 08:59:27 +0530

    Akshay | Initialised a branch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import "./App.css";
-import Header from "./components/header.jsx";
-import BodyCard from "./components/subCard";
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-  const [content, setContent] = useState({});
-
-  return (
-    <div className="App">
-      <Header total={content} />
-      <BodyCard updateGlobalState={(val) => setContent(val)} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
