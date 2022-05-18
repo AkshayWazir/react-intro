c3666a521ef84cb142c22089411b9b7436cd95d8
akshay.rein-at-908805445373 <waizaeer4@gmail.com>
2022-05-18 12:17:25 +0530

    axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import React from "react";
 import { Dashboard } from "./screens";
+import axios from "axios";
 
 export default function App() {
+  axios.get("http://localhost:4000/url/n").then(function (response) {
+    console.log("from App.js :", response.data);
+  });
+
   return (
     <div className="App">
       <Dashboard />
     </div>
   );
 }
+
+// * prop & functions flow is confusing
+// * JS
+// * Logic building
+// lifecycle
